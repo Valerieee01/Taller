cad2de412fcdfd5ec85865bad7aa58d58233f3e4
Valerieee01 <zharmel.afa@gmail.com>
2024-10-19 20:48:32 -0500

    Ejercicios_1_documentado

--- Ejercicio_1/app.js
+++ Ejercicio_1/app.js
@@ -1,3 +1,8 @@
+/**
+ * nTarjeta {int} numero de la tarjeta ingresada por el usuario
+ * verificaTrajeta exporta todos los metodos del modulo tarjetas
+ */
+
 import { tarjetas, verificarTrajeta } from "./tarjetas.js";
 
 
@@ -7,10 +12,10 @@ while (true) {
         if(isNaN(nTarjeta)){
             throw new Error("Caracter no permitido");
         } 
-        if (nTarjeta.length > 16) {
+        if (nTarjeta.length > 16) { // se evalia que no se ingren mas de 16 caracteres
             throw new Error("No es valido el numero error de caracteres");
         }
-        if (nTarjeta < 0) {
+        if (nTarjeta < 0) { // no recibe numeros negativos
             throw new Error("No se aceptan numeros negativos"); 
         }
     alert(`Excelente, tu tarjeta es ${verificarTrajeta(nTarjeta)}`);
--- Ejercicio_1/tarjetas.js
+++ Ejercicio_1/tarjetas.js
@@ -1,25 +1,35 @@
+/**
+ * @method america @returns {Boolean}
+ * @method diners @returns {Boolean}
+ * @method discover @returns {Boolean}
+ * @method masterCard @returns {Boolean}
+ * @method visa @returns {Boolean}
+ * @param {int} numero
+ * @module tarjetas @exports methods
+ */
+
 function america (numero) {
-    let exp = /^3[4-7]\d{13}$/;  // el elevado dice que inicia el pesos dice que termina
+    let exp = /^3[4-7]\d{13}$/;  // esta expresion regular evalua que no tenga mas de 15 digitos, que inicie en un numero de 34-37, y que sean solo numeros
     return exp.test(numero);
 }
 
 function diners(numero) {
-    let exp = /^3(?:0[0-5]|[68]\d)\d{11}$/
+    let exp = /^3(?:0[0-5]|[68]\d)\d{11}$/ // esta expresion regular evalua que tenga mas de 14 digitos, que empiece por  300 - 305, o 36 - 38 y sean solo numeros
     return exp.test(numero);
 }
 
 function discover(numero) {
-    let exp = /^6011\d{12}$/;
+    let exp = /^6011\d{12}$/; //esta expresion regular evalua que tenga 16 digitos que empiece en 6011 y sean solo numeros 
     return exp.test(numero);
 }
  // 16 digitos 51 - 55
 function masterCard(numero) {
-    let exp = /^5([1-5])\d{14}$/;
+    let exp = /^5([1-5])\d{14}$/; // esta expresion regular evalua que tenga 16 digitos, que empiece por 51-55 y solo sean numeros
     return exp.test(numero);
 }
 
 function visa(numero) {
-    let exp = /^4\d{15}$/;
+    let exp = /^4\d{15}$/; // esta expresion regular evalua que tenga 15 digitos, que empiece por 4 y solo sean numeros 
     return exp.test(numero);
 }
 
